a91dd066d4a32849e0ba3a9758c3724520fa33f6
Eddie Villarreal <eddie.villarreal@outlook.com>
2020-08-12 14:49:19 -0500

    added css

---
+++ script.js
@@ -0,0 +1,50 @@
+var queue = [{
+    "question":"What is an array?",
+    "ans1":"The best way to get an element by it's id",
+    "ans2":"It assigns value to a variable",
+    "ans3":"It is used to store multiple values in a single variable.",
+    "ans4":"A block of code used to perform a particular task.",
+    "answer":"3"
+},{
+    "question":"What is a function?",
+    "ans1":"It logs data to console.",
+    "ans2":"A block of code used to perform a particular task.",
+    "ans3":"It is used to store multiple values in a single variable.",
+    "ans4":"The bet way to get an element by it's id.",
+    "answer":"2"
+},{
+    "question":"What symbols are used to call a function?",
+    "ans1":"<>",
+    "ans2":"()",
+    "ans3":"**",
+    "ans4":"??", 
+    "answer":"2"
+},{
+    "question":"What does the function math.random() return?",
+    "ans1":"a random number between 0 and 1 not including 1",
+    "ans2":"a random number between 0 and 10 not including 10",
+    "ans3":"a random number between 0 and 100 not including 100",
+    "ans4":"a random number between 0 and 1000 not including 1000",
+    "answer":"1"
+},{
+    "question":"What value does array.length return?",
+    "ans1":"The length of the for loop.",
+    "ans2":"The length of the while loop",
+    "ans3":"The length of the math.random function.",
+    "ans4":"The length of the array.",
+    "answer":"4"
+
+}
+
+]
+
+
+
+
+
+var questNum = document.getElementById("questNum");
+var question = document.getElementById("ask");
+var ans1 = document.getElementById("ans1");
+var ans2 = document.getElementById("ans2");
+var ans3 = document.getElementById("ans3");
+var ans4 = document.getElementById("ans4");
